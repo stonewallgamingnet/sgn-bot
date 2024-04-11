05728f314433232414063a8dc49510adc9f3ffa0
Nick Swinford <NicholasJohn16@gmail.com>
2024-04-11 15:29:33 -0400

    add test users

--- test-users.js
+++ test-users.js
@@ -1,6 +1,6 @@
 exports.user1 = {
     email: 'nicholasjohn16+discord001@gmail.com',
-    username: 'testuser001#4428',
+    username: 'testuser001#4428', //potatovestal
     password: '5mJ3$oA9*ODmBtZC',
     user_id: '1030724466470309898'
 }
