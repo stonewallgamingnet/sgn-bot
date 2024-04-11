867de94870e7ada86c50d6e4d02258c2f5fe698c
Nick Swinford <NicholasJohn16@gmail.com>
2024-04-11 15:30:17 -0400

    add deploy commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,6 +2,16 @@ const { REST, Routes } = require('discord.js');
 require('dotenv').config();
 const fs = require('node:fs');
 
+/** sgnBot */
+// const GUILD_ID = "162955155875037184"; // sgn 
+// const CLIENT_ID = "377305806321877004"; //sgnBot
+// const DISCORD_TOKEN = "Mzc3MzA1ODA2MzIxODc3MDA0.DqGdgg.GbBjdGsYxsxfQjW7sAPFJzjoONM";
+
+/** devBot */
+GUILD_ID="375133424261791755"
+CLIENT_ID="500137506248065025"
+DISCORD_TOKEN="NTAwMTM3NTA2MjQ4MDY1MDI1.DqGeKw.yWjS2IHLkgUhKBdhn-YFULUu980"
+
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
 const commandFiles = fs.readdirSync('./app/commands').filter(file => file.endsWith('.js') && !file.includes('test'));
@@ -13,9 +23,9 @@ for (const file of commandFiles) {
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+const rest = new REST({ version: '10' }).setToken(DISCORD_TOKEN);
 
-rest.put(Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, process.env.DISCORD_GUILD_ID), { body: [] })
+rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: [] })
 	.then(() => console.log('Successfully deleted all guild commands.'))
 	.catch(console.error);
 
@@ -26,7 +36,7 @@ rest.put(Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, process.
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, process.env.DISCORD_GUILD_ID),
+			Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
 			{ body: commands },
 		);
 
