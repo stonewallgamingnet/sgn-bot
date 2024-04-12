be8143e681936e45a8176bb7ad99391195c6f15e
Joshua Lee <joshuajohnlee@me.com>
2024-04-12 14:13:46 +0100

    Transwarp command and related JSON added

---
+++ app/commands/transwarp.js
@@ -0,0 +1,58 @@
+const transwarp_data = require('../../transwarp_data.json');
+const transwarps = transwarp_data.transwarps;
+
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+
+	data: new SlashCommandBuilder()
+		.setName('transwarp')
+		.setDescription('Enter a destination in STO to find the nearest transwarp location.')
+        .addStringOption(option =>
+            option
+                .setName('destination')
+                .setDescription('Enter a destination to search for.')
+                .setRequired(true)
+        ),
+
+	async execute(interaction) {
+        let searchString = interaction.options.getString('destination');
+
+        // Special cases - search string will be changed to match the JSON data
+        if (searchString.toLowerCase().includes("ds9")) {
+            searchString = "Deep Space Nine";
+        }
+
+        if (searchString.toLowerCase().includes("earth")) {
+            searchString = "Sol";
+        }
+
+        // Iterate through the JSON data for matches
+        const matches = transwarps.filter((transwarp) => {
+            if (transwarp.destination.toLowerCase().includes(searchString.toLowerCase())) {
+                return true;
+            }
+            return false;
+        })
+
+        // Exceptions
+        if (matches == []) {
+            interaction.reply({ content: `No matches found for ${searchString}. Please check your spelling or rephrase your search.`, ephemeral: true})
+        }
+
+        if (matches.length > 5) {
+            interaction.reply({ content: `Too many matches found for "${searchString}". Please refine your search.`, ephemeral: true})
+        }
+
+        // Output the results
+        let results = `${searchString} matches the following destinations transwarps: \n`;
+
+        let result_counter = 1
+        matches.forEach((result) => {
+            results += `${result_counter}. ${result.destination} can be reached through ${result.missionname} in the ${result.episodename} episode.\n`;
+            result_counter++;
+        })
+
+        interaction.reply({ content: results, ephemeral: true})
+	},
+};
