4acd0bfd5b3cc7891e3b0c9141bed42071cd40c9
Nick Swinford <NicholasJohn16@gmail.com>
2024-05-08 18:36:55 -0400

    add contextmenucommands in interaction create

--- app/events/interactionCreate.js
+++ app/events/interactionCreate.js
@@ -3,20 +3,37 @@ const { Events } = require('discord.js');
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
-		// if (!interaction.isChatInputCommand()) return;
+		
+		if(interaction.isModalSubmit()) {
+			// const modal = interaction.client.modals.get(interaction.customId);
 
-		const command = interaction.client.commands.get(interaction.commandName);
+			// if(!modal) {
+			// 	console.error(`No modal matching ${interaction.customId} was found.`);
+			// 	return;
+			// }
 
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`);
-			return;
+			// try {
+			// 	await modal.execute(interaction);
+			// } catch(error) {
+			// 	console.error(`Error executing ${interaction.customId}`);
+			// 	console.error(error);
+			// }
 		}
 
-		try {
-			await command.execute(interaction);
-		} catch (error) {
-			console.error(`Error executing ${interaction.commandName}`);
-			console.error(error);
+		if (interaction.isChatInputCommand() || interaction.isContextMenuCommand()) {
+			const command = interaction.client.commands.get(interaction.commandName);
+	
+			if (!command) {
+				console.error(`No command matching ${interaction.commandName} was found.`);
+				return;
+			}
+	
+			try {
+				await command.execute(interaction);
+			} catch (error) {
+				console.error(`Error executing ${interaction.commandName}`);
+				console.error(error);
+			}
 		}
 	},
 };
