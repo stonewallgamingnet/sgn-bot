197f60a8a0177e45b950d71859f2c2ec5c12642c
Nick Swinford <NicholasJohn16@gmail.com>
2024-05-13 02:26:40 -0400

    add user by default on presence update

--- app/events/presenceUpdate.js
+++ app/events/presenceUpdate.js
@@ -3,8 +3,10 @@ const DiscordUser = require('../models/DiscordUser');
 
 module.exports = {
     name: Events.PresenceUpdate,
-    async execute(oldPresence, newPresence) {
-        const discordUser = await DiscordUser.findByUserId(newPresence.userId);
+    async execute(oldPresence, newPresence) {     
+        const discordUser = await DiscordUser.findOrAddNew(newPresence.member);
+
+        if(!discordUser) { return; }
 
         discordUser.data.presence = newPresence.status;
 
